68ada78ad0fce1ff2753c83496128ff2e4b7b73b
ahinestrog <124910059+ahinestrog@users.noreply.github.com>
2024-04-16 20:36:28 -0500

    Version2_input

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 def input_cky():
     nCasos = int(input())
     for i in range(nCasos):
-        tc = list(map(int, input().split()))
+        l = input()
+        nt_strings = [int(i) for i in l.split()]
         G = {}
-        terminals = 0
-        while terminals < tc[0]:
-            l = input().split()
+        nonterminals = 0
+        while nonterminals < nt_strings[0]:
+            l = input()
+            l = l.split()
             G[l[0]] = []
             for j in range(1, len(l)):
                 G[l[0]].append(l[j])
-            terminals+=1
-            strings = 0
-            while strings < tc[1]:
-                string = input()
-                #ans = cky(G, L)
-                if 0 :
-                    print("yes")
-                else:
-                    print("no")
-                i+=1
+            nonterminals += 1
+        strings = 0
+        while strings < nt_strings[1]:
+            l = input()
+            #ans = cky(G, l)
+            if 0:
+                print('yes')
+            else:
+                print('no')
+            strings += 1
 
 def cky():
     i = 0
